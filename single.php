4cc410f2c0fcfc39e6aee264b53af4674ad55173
Feliks N <feliks.nvk@gmail.com>
2026-02-11 17:19:50 +0100

    Почистить файл отдельной записи от ненужной информации

--- single.php
+++ single.php
@@ -1,25 +1,16 @@
 <?php get_header(); ?>
 
-<!-- Рекомендация: использовать функцию the_title() внутри цикла, а для заголовка страницы использовать single_post_title() вне цикла. -->
-
-<!-- Название Записи пределами цикла -->
-<?php single_post_title(); ?>
-
-<?php if( have_posts() ) { ?>
-    <?php while( have_posts() ) { ?>
-        <?php the_post(); ?>
-        
-        <!--миниатюра (размер миниатюры - thumbnail)-->
-        <?php the_post_thumbnail('thumbnail'); ?>
-        
-        <!-- Название записи внутри цикла -->
-        <?php the_title() ?>
-        
-        <!--контент записи ( то что в редакторе написано )-->
-        <?php the_content()?>
+<div class="container">
+    <?php if( have_posts() ) { ?>
+        <?php while( have_posts() ) { ?>
+            <?php the_post(); ?>
+            
+            <?php the_post_thumbnail('medium'); ?>
+            <h1 class="heading"><?php the_title(); ?></h1>
+            <?php the_content()?>
+            
+        <?php } ?>
     <?php } ?>
-<?php } ?>
-
-
+</div>
 
 <?php get_footer(); ?>
