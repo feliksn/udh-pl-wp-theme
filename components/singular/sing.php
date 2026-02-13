4ce1470ae28b542a2ab9717890684a6445f86b25
vieri3 <egordohlyj4@gmail.com>
2026-02-13 19:38:52 +0300

    Позиционирование элементов на стр. продукт

--- components/singular/sing.php
+++ components/singular/sing.php
@@ -1,40 +1,23 @@
- <div class="d-flex">
-
-     <?php the_post_thumbnail('medium'); ?>
-
-     <div>
-         <?php
-
-            $args = array(
-                'post_type'      => 'product',
-                'posts_per_page' => -1,          // вывести все посты
-                'tax_query' => array([
-                    'taxonomy' => 'category', // таксономия-категория
-                    'field'    => 'slug',
-                    'terms'    => 'piwo',     // slug вашей категории
-                ]),
-            );
-
-            $query = new WP_Query($args);
-
-            if ($query->have_posts()) {
-                while ($query->have_posts()) {
-                    $query->the_post();
-            ?>
-
-                 <a href="<?php the_permalink(); ?>" class="m-3">
-                     <?php the_post_thumbnail('thumbnail'); ?>
-                 </a>
-
-         <?php }
-                wp_reset_postdata();
-            } ?>
-
-     </div>
-
- </div>
-
-
- <h1 class="heading"><?php the_title(); ?></h1>
-
- <?php the_content() ?>
+<div class="d-flex">
+    <?php
+        $args = array(
+            'post_type'      => 'product',
+            'posts_per_page' => -1,          // вывести все посты
+            'tax_query' => array([
+                'taxonomy' => 'category', // таксономия-категория
+                'field'    => 'slug',
+                'terms'    => 'piwo',     // slug вашей категории
+            ]),
+        );
+        $query = new WP_Query($args);
+    ?>
+    <?php if ($query->have_posts()) : ?>
+        <?php while ($query->have_posts()) : ?>
+            <?php $query->the_post(); ?>
+            <a href="<?php the_permalink(); ?>" class="mx-3">
+                <?php the_post_thumbnail('thumbnail'); ?>
+            </a>
+        <?php endwhile; ?>
+        <?php wp_reset_postdata(); ?>
+    <?php endif ?>
+</div>
--- singular.php
+++ singular.php
@@ -1,16 +1,25 @@
 <?php get_header('single'); ?>
 
 <div class="container">
-    <?php if (have_posts()) { ?>
-        <?php while (have_posts()) { ?>
+    <?php if (have_posts()) : ?>
+        <?php while (have_posts()) : ?>
             <?php the_post(); ?>
 
+            <div class="row">
+                <div class="col text-end">
+                    <h3><?php the_title(); ?></h3>
+                    <?php the_content(); ?>    
+                </div>
+                <div class="col text-center">
+                    <?php the_post_thumbnail('medium'); ?>
+                </div>
+                <div class="col">
+                    <?php get_template_part('components/singular/sing'); ?>
+                </div>
+            </div>
 
-            <?php get_template_part('components/singular/sing'); ?>
-
-
-        <?php } ?>
-    <?php } ?>
+        <?php endwhile; ?>
+    <?php endif; ?>
 </div>
 
 <?php get_footer(); ?>
