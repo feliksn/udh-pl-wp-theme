083922bf2f27e71cbf62a57e4a91a46eb1f764b7
vieri3 <egordohlyj4@gmail.com>
2026-02-15 11:40:18 +0300

    переименование компонента в понятное название

--- singular.php
+++ singular.php
@@ -6,15 +6,15 @@
             <?php the_post(); ?>
 
             <div class="row">
-                <div class="col text-end">
+                <div class="col-5 text-end ps-5">
                     <h3><?php the_title(); ?></h3>
                     <?php the_content(); ?>    
                 </div>
-                <div class="col text-center">
+                <div class="col-2 text-center">
                     <?php the_post_thumbnail('medium'); ?>
                 </div>
-                <div class="col">
-                    <?php get_template_part('components/singular/sing'); ?>
+                <div class="col-5">
+                    <?php get_template_part('components/block-product/block-product'); ?>
                 </div>
             </div>
 
