7cdd5a4ef8b27da34328b61e9be629de7cd44e0a
vieri3 <egordohlyj4@gmail.com>
2026-02-15 15:35:54 +0300

    добавление и стилизация блока статистики

---
+++ components/block-statistics/block-statistics.php
@@ -0,0 +1,23 @@
+<div class="row row-cols-lg-4 row-cols-sm-2 row-cols-xs-1 text-center">
+
+    <div>
+        <div class="productCounter-num">4.7<span>%</span></div>
+        <div class="productCounter-txt">zawartość alkoholu </div>
+    </div>
+
+    <div>
+        <div class="productCounter-num">10.75<span>%</span></div>
+        <div class="productCounter-txt">zawartość ekstraktu </div>
+    </div>
+
+    <div>
+        <div class="productCounter-num">330<span>ml</span></div>
+        <div class="productCounter-txt">pojemność </div>
+    </div>
+
+    <div>
+        <div class="productCounter-num">24<span>szt.</span></div>
+        <div class="productCounter-txt">sztuk w opakowaniu </div>
+    </div>
+
+</div>
--- singular.php
+++ singular.php
@@ -6,20 +6,30 @@
             <?php the_post(); ?>
 
             <div class="row">
-                <div class="col-5 text-end ps-5">
+
+                <div class="col-5 d-flex flex-column text-end align-items-end ps-5">
+                    <!-- заглушка для картинки брэнда -->
+                    <div style="width: 119px; height: 93px; border-radius: 40px; background-color: tomato; text-align: center; line-height: 93px">
+                        pic prod
+                    </div>
                     <h3><?php the_title(); ?></h3>
                     <?php the_content(); ?>    
                 </div>
-                <div class="col-2 text-center">
+
+                <div class="col-2 d-flex justify-content-center align-self-end">
                     <?php the_post_thumbnail('medium'); ?>
                 </div>
                 <div class="col-5">
-                    <?php get_template_part('components/block-product/block-product'); ?>
+                    <?php get_template_part('components/block-product/block-pics-links-products'); ?>
                 </div>
             </div>
 
         <?php endwhile; ?>
     <?php endif; ?>
+
+    <!-- блок с статистикой (данные про продукт) -->
+    <?php get_template_part('components/block-statistics/block-statistics'); ?>
+
 </div>
 
 <?php get_footer(); ?>
