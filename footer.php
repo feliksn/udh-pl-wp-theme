6ff7a294ae1a0d80fb626cf7967c53f9750e3d52
vieri3 <egordohlyj4@gmail.com>
2026-02-18 22:33:33 +0300

    добавлен блок newsletter+social

--- footer.php
+++ footer.php
@@ -4,6 +4,10 @@
 
     <?php get_template_part('components/block-pics-liks-brands/block-pics-liks-brands'); ?>
 
+    <?php get_template_part('components/block-newsletter-social/block-newsletter-social'); ?>
+
+    <div style="padding-top: 250px"></div>
+  
 </footer>
 <!-- Нужна база данных с записями постами метками меню -->
 <?php wp_footer(); ?>
