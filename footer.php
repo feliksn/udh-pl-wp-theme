f1c065a8f81d983acbc682277d2c30432ead0492
vieri3 <egordohlyj4@gmail.com>
2026-02-18 11:36:54 +0300

    работа с footer

--- footer.php
+++ footer.php
@@ -1,5 +1,9 @@
 <footer class="footer">
 
+    <?php get_template_part('components/block-statistics-footer/block-statistics-footer'); ?>
+
+    <?php get_template_part('components/block-pics-liks-brands/block-pics-liks-brands'); ?>
+
 </footer>
 <!-- Нужна база данных с записями постами метками меню -->
 <?php wp_footer(); ?>
