f19087383c24956818f6d103fcd9a30ed022adcd
vieri3 <egordohlyj4@gmail.com>
2026-02-18 11:45:30 +0300

    выносим в отдельный файл post types fn

--- functions.php
+++ functions.php
@@ -96,66 +96,6 @@ function disable_comments_admin_menu_redirect() {
    	}
 }
 
-// Register custom post types
-add_action( 'init', 'register_post_types' );
-function register_post_types(){
-	// brand
-	register_post_type( 'brand', [
-		'label'  => null,
-		'labels' => [
-			'name'               => 'Marki',
-			'singular_name'      => 'Marka',
-			'add_new'            => 'Dodaj markę',
-			'add_new_item'       => 'Dodaj markę',
-			'edit_item'          => 'Edytuj markę',
-			'new_item'           => 'Nowa marka',
-			'view_item'          => 'Oglądaj markę',
-			'search_items'       => 'Szukaj marki',
-			'not_found'          => 'Nie znaleziono',
-			'not_found_in_trash' => 'Nie znaleziono w koszu',
-			'menu_name'          => 'Marka',
-		],
-		'public'              => true,
-		'show_in_menu'        => true,
-		'show_in_admin_bar'   => false,
-		'show_in_rest'        => true,
-		'menu_position'       => 20,
-		'menu_icon'           => 'dashicons-awards',
-		'hierarchical'        => true,
-		'supports'            => array('title', 'editor', 'page-attributes', 'thumbnail'),
-	] );
-	
-	// product
-	register_post_type( 'product', [
-		'label'  => null,
-		'labels' => [
-			'name'               => 'Produkty',
-			'singular_name'      => 'Produkt',
-			'add_new'            => 'Dodaj produkt',
-			'add_new_item'       => 'Dodaj produkt',
-			'edit_item'          => 'Edytuj produkt',
-			'new_item'           => 'Nowy produkt',
-			'view_item'          => 'Oglądaj produkt',
-			'search_items'       => 'Szukaj produkt',
-			'not_found'          => 'Nie znaleziono',
-			'not_found_in_trash' => 'Nie znaleziono w koszu',
-			'menu_name'          => 'Produkt',
-		],
-		'public'              => true,
-		'show_in_menu'        => true,
-		'show_in_admin_bar'   => false,
-		'show_in_rest'        => true,
-		'menu_position'       => 21,
-		'menu_icon'           => 'dashicons-products',
-		'hierarchical'        => false,
-		'supports'            => array('title', 'editor', 'page-attributes', 'thumbnail'),
-		'taxonomies'          => [ 'category' ],
-		'has_archive'         => true,
-		'rewrite'             => true,
-	] );
-}
-
-
 
 add_filter( 'the_excerpt', 'limit_excerpt_length' );
 function limit_excerpt_length( $post_excerpt ) {
--- require/cpt.php
+++ require/cpt.php
@@ -1,6 +1,63 @@
 <?php
 
+// Register custom post types
 add_action('init', function () {
+
+    // brand
+    register_post_type('brand', [
+        'label'  => null,
+        'labels' => [
+            'name'               => 'Marki',
+            'singular_name'      => 'Marka',
+            'add_new'            => 'Dodaj markę',
+            'add_new_item'       => 'Dodaj markę',
+            'edit_item'          => 'Edytuj markę',
+            'new_item'           => 'Nowa marka',
+            'view_item'          => 'Oglądaj markę',
+            'search_items'       => 'Szukaj marki',
+            'not_found'          => 'Nie znaleziono',
+            'not_found_in_trash' => 'Nie znaleziono w koszu',
+            'menu_name'          => 'Marka',
+        ],
+        'public'              => true,
+        'show_in_menu'        => true,
+        'show_in_admin_bar'   => false,
+        'show_in_rest'        => true,
+        'menu_position'       => 20,
+        'menu_icon'           => 'dashicons-awards',
+        'hierarchical'        => true,
+        'supports'            => array('title', 'editor', 'page-attributes', 'thumbnail'),
+    ]);
+
+    // product
+    register_post_type('product', [
+        'label'  => null,
+        'labels' => [
+            'name'               => 'Produkty',
+            'singular_name'      => 'Produkt',
+            'add_new'            => 'Dodaj produkt',
+            'add_new_item'       => 'Dodaj produkt',
+            'edit_item'          => 'Edytuj produkt',
+            'new_item'           => 'Nowy produkt',
+            'view_item'          => 'Oglądaj produkt',
+            'search_items'       => 'Szukaj produkt',
+            'not_found'          => 'Nie znaleziono',
+            'not_found_in_trash' => 'Nie znaleziono w koszu',
+            'menu_name'          => 'Produkt',
+        ],
+        'public'              => true,
+        'show_in_menu'        => true,
+        'show_in_admin_bar'   => false,
+        'show_in_rest'        => true,
+        'menu_position'       => 21,
+        'menu_icon'           => 'dashicons-products',
+        'hierarchical'        => false,
+        'supports'            => array('title', 'editor', 'page-attributes', 'thumbnail'),
+        'taxonomies'          => ['category'],
+        'has_archive'         => true,
+        'rewrite'             => true,
+    ]);
+
     // news
     register_post_type('news', [
         'label'  => null,
@@ -27,5 +84,6 @@ add_action('init', function () {
         'supports'            => array('title', 'editor', 'page-attributes', 'thumbnail'),
     ]);
 });
+
 
 ?>
