ba7a9c4bae65261b9b01e23724e8ae715c6d817d
Feliks N <feliks.nvk@gmail.com>
2026-02-23 17:29:18 +0100

    Стилизировать шапку сайта под актуальную тему

--- header.php
+++ header.php
@@ -11,20 +11,19 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
-    <!--Присваиваем переменной функцию котора я будет в дальнейшем выводить из админки номери почту в нужных местах-->
-    <?php $udh_pl_wp_theme_options = udh_pl_wp_theme_options(); ?>
 </head>
 
 <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
-    <header class="mb-3 border-bottom">
+    
+    <header class="header helper-bg-image-cover-center">
         <div class="container">
             <?php
-            wp_nav_menu([
-                'container' => '',
-                'menu_id' => 'menu-list',
-                'menu_class' => 'nav list-unstyled'
-            ]);
+                wp_nav_menu([
+                    'container' => '',
+                    'menu_id' => 'menu-list',
+                    'menu_class' => 'nav list-unstyled justify-content-center'
+                ]);
             ?>
         </div>
 
