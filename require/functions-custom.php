a8c4ffc9a685599f2208f68ec4aa2ffd2908b098
Feliks N <feliks.nvk@gmail.com>
2026-02-23 17:37:37 +0100

    Создать функции для подключения стилей и функций в разных местах сайта для разпределения загрузки функциональности

--- require/functions-custom.php
+++ require/functions-custom.php
@@ -1,8 +1,23 @@
 <?php
 
 // Display data in the var
-function _var( $var ){
-    echo '<pre>' . print_r( $var, true ) . '</pre>';
+function _var( $_var ){
+    echo '<pre>' . print_r( $_var, true ) . '</pre>';
+}
+
+/**
+ * Get template functions for dedicated place
+ */
+function _get_template_fn($_name){
+    require_once THEME_PATH . "/templates/$_name/$_name-fn.php";
+}
+
+/**
+ * Get template styles for dedicated place
+ */
+function _get_template_css($_name){
+    $theme_uri = THEME_URI;
+    echo "\n<style>@import url('$theme_uri/templates/$_name/$_name.css')</style>";
 }
 
 
