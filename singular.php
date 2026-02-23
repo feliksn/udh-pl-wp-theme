3810965154c69443e853b3b8cadeccac18d58ade
Feliks N <feliks.nvk@gmail.com>
2026-02-23 17:16:04 +0100

    Использовать шаблон для страниц и других отдельных типов записей, за исключением продукта

--- singular.php
+++ singular.php
@@ -2,12 +2,6 @@
 
 <div class="container">
     
-    <!-- блок с основной картинкой, описанием, лого и пр. продукта -->
-   	<?php get_template_part('components/block-product/block-product'); ?>
-
-    <!-- блок с статистикой (данные про продукт) -->
-    <?php get_template_part('components/block-statistics/block-statistics'); ?>
-
 </div>
 
 <?php get_footer(); ?>
